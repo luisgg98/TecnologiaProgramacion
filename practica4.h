ecd63490e0b9c16df30028fa5f770a620ab1f74d
saul205 <45789194+saul205@users.noreply.github.com>
2019-04-30 14:07:14 +0200

    Add files via upload

---
+++ practica4.h
@@ -0,0 +1,107 @@
+
+#ifndef OBJETO_H
+#define OBJETO_H
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Objeto{
+
+    protected:
+        double capacidad;
+
+    public:
+
+        Objeto( double cap ) : capacidad(cap){};
+
+        double capacidad_(){
+            return capacidad;
+        }
+
+};
+
+class Generico : public Objeto {
+
+    public:
+
+        Generico( double cap ) : Objeto(cap){};
+
+};
+
+class ProductoG : public Objeto {
+
+    string Nombre;
+
+    public:
+
+        ProductoG( double cap, string name ) : Objeto(cap), Nombre(name) {};
+
+};
+
+class Toxico : public ProductoG {
+
+    public:
+
+        Toxico( double cap, string name ) : ProductoG(cap, name) {};
+
+};
+
+class SerVivo : public ProductoG {
+
+    public:
+
+        SerVivo( double cap, string name ) :  ProductoG(cap, name) {};
+
+};
+
+
+class Producto : public ProductoG, public Generico {
+
+    public:
+
+        Producto( double cap, string name ) : ProductoG(cap, name), Generico(cap) {};
+
+};
+
+template <typename T>
+class Carga : public Objeto {
+
+    public:
+
+        Carga( double cap ) : Objeto(cap) {};
+
+        virtual bool guardar(T element){
+            
+            if ( Objeto::capacidad - element.capacidad_() < 0 ){
+                return false;
+            }
+            else
+            {   
+                Objeto::capacidad -= element.capacidad_();
+                return true;
+            }
+
+        };
+
+};
+
+template <typename T>
+class Contenedor : public Generico, public Carga<T> {
+
+    public:
+
+        Contenedor( double cap ) : Generico(cap), Carga<T>(cap) {};
+
+};
+
+class Camion : public Carga<Generico> {
+
+    public:
+
+        Camion( double cap ) : Carga<Generico>(cap) {};
+
+};
+
+#endif
