fc5307227349436f74d6901870c13dae8745da04
Luis Garcia <45603226+luisgg98@users.noreply.github.com>
2019-05-19 19:30:45 +0200

    Practica 6 (una funcion no compila)

---
+++ BinaryTree.hs
@@ -0,0 +1,44 @@
+--Autores:
+----Saúl Flores Benavente 755769
+----Luis García Garcés 739202
+module BinaryTree where
+
+data Tree a = EmptyTree | Branch a (Tree a) (Tree a) deriving Show
+
+empty  :: Tree a 
+empty = EmptyTree
+
+leaf :: a -> Tree a
+leaf x = Branch x (empty) (empty)
+
+tree :: a ->  a -> a -> Tree a --Da problemas
+tree  x lb rb = Branch x (leaf lb) (leaf rb)
+
+size :: Tree a -> Float
+size EmptyTree = 0
+size (Branch x l d) = 1+size l + size d
+
+preorder :: Tree a -> [a]
+preorder EmptyTree = []
+preorder (Branch x l d)= [x] ++ preorder l ++preorder d
+
+postorder :: Tree a -> [a]
+postorder EmptyTree = []
+postorder (Branch x l d)= postorder l ++postorder d ++ [x]  
+
+inorder :: Tree a -> [a]
+inorder EmptyTree = []
+inorder (Branch x l d)= inorder l ++ [x] ++ inorder d
+ 
+add :: Ord a => Tree a -> a -> Tree a
+add EmptyTree x = leaf x
+add (Branch p l d) x 
+    | x < p = add l x
+    | x >= p = add d x
+
+between :: Ord a =>  Tree a -> a -> a -> [a]
+between (Branch x l d) xmin xmax
+    | xmin >= x && x < xmax = [x] ++ between l xmin xmax ++ between d xmin xmax
+    | x > xmax = between l xmin xmax
+    | x < xmin = between d xmin xmax
+    | otherwise = []
